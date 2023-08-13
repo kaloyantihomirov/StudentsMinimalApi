b72cd24957d556d194b14d11404754702aadb224
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-13 23:11:24 +0300

    Added a validation helper class

---
+++ StudentsMinimalApi/ValidationHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentsMinimalApi
+{
+    public class ValidationHelper 
+    {
+        public static async ValueTask<object?> ValidateId(
+            EndpointFilterInvocationContext context,
+            EndpointFilterDelegate next)
+        {
+            string id = context.GetArgument<string>(0);
+
+            return null;
+        }
+
+
+    }
+}
