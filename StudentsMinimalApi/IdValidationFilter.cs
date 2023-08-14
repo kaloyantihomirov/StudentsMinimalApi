a6084ac34f429ebe86e3e8dcc399a91ac19aed42
kaloyantk <kaloyan_tihomirov@icloud.com>
2023-08-14 22:25:59 +0300

    Added a class that implements the IEndpointFilter interface, so I've basically given three ways that we can extract our filter logic and probably reuse it.

---
+++ StudentsMinimalApi/IdValidationFilter.cs
@@ -0,0 +1,22 @@
+﻿namespace StudentsMinimalApi
+{
+    public class IdValidationFilter : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(
+            EndpointFilterInvocationContext context, 
+            EndpointFilterDelegate next)
+        {
+            string id = context.GetArgument<string>(0);
+
+            if (string.IsNullOrEmpty(id) || !id.StartsWith('s'))
+            {
+                return Results.ValidationProblem(new Dictionary<string, string[]>
+                {
+                     { "id", new[] { "Id cannot be null or empty and must start with 's'" } }
+                });
+            }
+
+            return await next(context);
+        }
+    }
+}
